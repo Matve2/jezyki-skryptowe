3dbe55f71da6a20ee3fa2f5f92184dec50468fe8
Mateusz Ciszek <matckf@gmail.com>
2020-11-11 20:09:22 +0100

    sortowanie 1 listy

---
+++ sortowanie.py
@@ -0,0 +1,5 @@
+list_to_sort=[[3,2,3], [2,0,2], [3,0,1], [3,1,0]]
+
+sorted_list=sorted(list_to_sort, key=lambda list_element:(list_element[1], list_element[2]))
+print("Lista 1 przed sortowaniem: ", list_to_sort)
+print("Lista 1 po sortowaniu: ", sorted_list)
