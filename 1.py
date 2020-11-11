30a2d866bf9a94fd9c6948e709606841696fa0d8
Mateusz Ciszek <mateusz.ciszek@student.up.krakow.pl>
2020-11-11 20:01:57 +0100

    gotowe

--- 1.py
+++ 1.py
@@ -2,8 +2,10 @@ list_to_sort=[[3,2,3], [2,0,2], [3,0,1], [3,1,0]]
 list_to_sort2=[[3,2,3], [2,0,2], [3,0,1], [3,1,0]]
 
 sorted_list=sorted(list_to_sort, key=lambda list_element:(list_element[1], list_element[2]))
-print(list_to_sort)
+print("Lista 1 przed sortowaniem: ", list_to_sort)
+print("Lista 1 po sortowaniu: ", sorted_list)
 
 
-
-print(sorted_list)
+sorted_list_2=sorted(list_to_sort2, key=lambda list_element2:(list_element2[1], list_element2[2]))
+print("Lista 2 przed sortowaniem: ", list_to_sort2)
+print("Lista 2 po sortowaniu: ", sorted_list_2)
